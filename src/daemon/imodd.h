26b2fc4b3e5eed9797cbecb10279df1daad656f6
tildearrow <tildearrow@users.noreply.github.com>
2018-11-13 15:41:34 -0500

    bump version to 1.0.1

--- src/daemon/imodd.h
+++ src/daemon/imodd.h
@@ -36,7 +36,7 @@ typedef std::string string;
 
 #define logLevel 3
 
-#define IMOD_VERSION "1.0"
+#define IMOD_VERSION "1.0.1"
 
 int imLogD(const char* format, ...);
 int imLogI(const char* format, ...);
