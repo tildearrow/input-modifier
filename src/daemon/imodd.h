957173ee84a9b74fd26b52ae185950ac550504fc
tildearrow <tildearrow@users.noreply.github.com>
2018-12-07 14:47:05 -0500

    mark 1.3 release
    this is 1.3 because of the major installer change

--- src/daemon/imodd.h
+++ src/daemon/imodd.h
@@ -34,9 +34,9 @@ typedef std::string string;
 #define LOGLEVEL_INFO 2
 #define LOGLEVEL_DEBUG 3
 
-#define logLevel 3
+#define logLevel 2
 
-#define IMOD_VERSION "1.2"
+#define IMOD_VERSION "1.3"
 
 int imLogD(const char* format, ...);
 int imLogI(const char* format, ...);
