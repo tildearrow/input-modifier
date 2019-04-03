a34dc93a6dc1b14d192ed9fa2932afe8359f282d
tildearrow <tildearrow@users.noreply.github.com>
2019-04-03 15:30:11 -0500

    more microbus work
    now probably prints arguments and stuff

--- src/daemon/plugin/razer/razer.h
+++ src/daemon/plugin/razer/razer.h
@@ -13,6 +13,7 @@ class microBus {
   DBusMessage* msg;
   DBusMessageIter args;
   DBusMessage* reply;
+  DBusMessageIter replyArgs;
   unsigned int replyIndex;
   string name;
   bool errorInited;
@@ -23,6 +24,70 @@ class microBus {
   public:
     bool connect();
     bool requestName(string name);
+    void printArg(int type, DBusBasicValue& what) {
+      switch (type) {
+        case DBUS_TYPE_BYTE:
+          imLogD("- char %d\n",what.byt);
+          break;
+        case DBUS_TYPE_BOOLEAN:
+          imLogD("- bool %s\n",what.bool_val?"true":"false");
+          break;
+        case DBUS_TYPE_INT16:
+          imLogD("- short %d\n",what.i16);
+          break;
+        case DBUS_TYPE_UINT16:
+          imLogD("- unsigned short %d\n",what.u16);
+          break;
+        case DBUS_TYPE_INT32:
+          imLogD("- int %d\n",what.i32);
+          break;
+        case DBUS_TYPE_UINT32:
+          imLogD("- unsigned int %d\n",what.u32);
+          break;
+        case DBUS_TYPE_INT64:
+          imLogD("- long %lcd\n",what.i64);
+          break;
+        case DBUS_TYPE_UINT64:
+          imLogD("- unsigned long %ld\n",what.u64);
+          break;
+        case DBUS_TYPE_DOUBLE:
+          imLogD("- double %f\n",what.dbl);
+          break;
+        case DBUS_TYPE_STRING:
+          imLogD("- string %s\n",what.str);
+          break;
+        case DBUS_TYPE_OBJECT_PATH:
+          imLogD("- object %s\n",what.str);
+          break;
+        case DBUS_TYPE_SIGNATURE:
+          imLogD("- sign %s\n",what.str);
+          break;
+        case DBUS_TYPE_UNIX_FD:
+          imLogD("- fd %d\n",what.i32);
+          break;
+        default:
+          imLogD("- ???\n");
+          break;
+      }
+    }
+    void processRecursion(DBusMessageIter* it) {
+      DBusMessageIter anoth;
+      DBusBasicValue arg;
+      int argType;
+      imLogD("{\n");
+      do {
+        // get argument
+        argType=dbus_message_iter_get_arg_type(it);
+        if (argType==DBUS_TYPE_ARRAY || argType==DBUS_TYPE_STRUCT) {
+          dbus_message_iter_recurse(it,&anoth);
+          processRecursion(&anoth);
+        } else {
+          dbus_message_iter_get_basic(it,&arg);
+          printArg(argType,arg);
+        }
+      } while (dbus_message_iter_next(it));
+      imLogD("}\n");
+    }
     template<typename... A> bool call(string who, string where, string what, string method, A&&... arg) {
       msg=dbus_message_new_method_call(who.c_str(),where.c_str(),what.c_str(),method.c_str());
       if (msg==NULL) {
@@ -30,7 +95,7 @@ class microBus {
         return false;
       }
 
-      // TODO: arguments
+      dbus_message_iter_init_append(msg,&args);
       nothing(addArg(arg)...);
     
       reply=dbus_connection_send_with_reply_and_block(conn,msg,2000,&error);
@@ -38,6 +103,11 @@ class microBus {
       if (reply==NULL) {
         imLogW("reply is null.\n");
         return false;
+      }
+      if (dbus_message_iter_init(reply,&replyArgs)) {
+        processRecursion(&replyArgs);
+      } else {
+        imLogD("no arguments\n");
       }
      
       imLogI("success...\n");
